f81172dd878a534bef6f8135269cbc57f0cd5fc3
Ziya SARIKAYA <sarikayaziya@gmail.com>
2014-07-09 01:11:54 +0300

    Create project structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pyley',
+    version='0.1.0',
+    packages=['pyley', 'tests'],
+    url='https://github.com/ziyasal/pyley',
+    license='LICENSE',
+    author='ziyasal',
+    author_email='sarikayaziya@gmail.com',
+    description='Python client for an open-source graph database Cayley',
+    install_requires=[''],
+    include_package_data=True
+)
---
+++ tests/gremlin_query_tests.py
@@ -0,0 +1,17 @@
+__author__ = 'ziyasal'
+
+from unittest import TestCase
+
+
+class gremlin_query_tests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print(0)
+
+
+    def graph_vertex_query_test(self):
+        self.assertEqual(True, True)
+
+    @classmethod
+    def tearDownClass(cls):
+        print(0)
