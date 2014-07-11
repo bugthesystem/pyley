122ba6d8c6cc28e3b9c1d7abd942d51409cd70cf
Ziya SARIKAYA <sarikayaziya@gmail.com>
2014-07-11 14:49:56 +0300

    Add package details

--- pyley/__init__.py
+++ pyley/__init__.py
@@ -1,3 +1,16 @@
+"""
+pyley Python client for an open-source graph database Cayley
+
+:copyright: (c) 2014 by Ziya SARIKAYA @ziyasal.
+:license: MIT, see LICENSE for more details.
+
+"""
+
+__title__ = 'pyley'
 __version__ = '0.1.0'
+__build__ = 0x00001
+__author__ = 'Ziya SARIKAYA @ziyasal'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014 Ziya SARIKAYA @ziyasal'
 
 VERSION = tuple(map(int, __version__.split('.')))
