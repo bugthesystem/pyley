3f6ab7613c8f4a8f96ea61acac506cb75121bc06
Ziya SARIKAYA <sarikayaziya@gmail.com>
2014-08-04 17:01:52 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyley',
-    version='0.1.0',
+    version='0.1.1-dev',
     packages=['tests'],
     scripts=['pyley.py'],
     url='https://github.com/ziyasal/pyley',
