cfc480fdfafa6bcf10f32efba0309e93f38f7716
Abdullah Selek <abdullahselek@gmail.com>
2018-01-07 18:31:08 +0000

    Update cayley version for ci

--- .travis.sh
+++ .travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget https://github.com/google/cayley/releases/download/v0.4.0/cayley_0.4.0_linux_amd64.tar.gz
-tar -xvzf cayley_0.4.0_linux_amd64.tar.gz
-cd cayley_0.4.0_linux_amd64
+wget https://github.com/cayleygraph/cayley/releases/download/v0.7.0/cayley_0.7.0_linux_amd64.tar.gz
+tar -xvzf cayley_0.7.0_linux_amd64.tar.gz
+cd cayley_0.7.0_linux_amd64
 ./cayley http --dbpath=30kmoviedata.nq.gz &
 sleep 2
