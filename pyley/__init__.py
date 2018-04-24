0fe221f01cc99e146d1fe223af022af9bf1dd1a9
Abdullah Selek <abdullahselek@gmail.com>
2018-04-24 20:57:15 +0100

    Update pypi download address.

--- pyley/__init__.py
+++ pyley/__init__.py
@@ -6,7 +6,7 @@ __author__ = 'Ziya SARIKAYA @ziyasal'
 __license__ = 'MIT License'
 __copyright__ = 'Copyright 2014 Ziya SARIKAYA @ziyasal'
 __url__          = 'https://github.com/ziyasal/pyley'
-__download_url__ = 'https://pypi.python.org/pypi/pyley'
+__download_url__ = 'https://pypi.org/pypi/pyley'
 __description__  = 'Python client for an open-source graph database Cayley'
 
 from .pyley import (
