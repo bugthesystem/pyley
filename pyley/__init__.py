f4b5f0140720dc5b8d0b23ac419d8665e28fae89
Abdullah Selek <abdullahselek@gmail.com>
2018-05-28 08:45:38 +0100

    Update init for details.

--- pyley/__init__.py
+++ pyley/__init__.py
@@ -1,10 +1,11 @@
 """pyley Python client for an open-source graph database Cayley"""
 
-__title__ = 'pyley'
-__version__ = '0.2.2'
-__author__ = 'Ziya SARIKAYA @ziyasal'
-__license__ = 'MIT License'
-__copyright__ = 'Copyright 2014 Ziya SARIKAYA @ziyasal'
+__title__        = 'pyley'
+__version__      = '0.2.2'
+__author__       = 'Ziya SARIKAYA @ziyasal'
+__email__        = 'sarikayaziya@gmail.com',
+__license__      = 'MIT License'
+__copyright__    = 'Copyright 2014 Ziya SARIKAYA @ziyasal'
 __url__          = 'https://github.com/ziyasal/pyley'
 __download_url__ = 'https://pypi.org/pypi/pyley'
 __description__  = 'Python client for an open-source graph database Cayley'
