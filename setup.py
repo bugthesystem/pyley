1d0eba72896e0a7ddd9151c9a286dd028e9fe2e4
Abdullah Selek <abdullahselek@gmail.com>
2018-05-28 08:51:58 +0100

    Easy update setup file which is ready for pypi.

--- setup.py
+++ setup.py
@@ -1,16 +1,54 @@
-__author__ = 'ziyasal'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function
 
-from setuptools import setup
+import os
+import re
+import codecs
+
+from setuptools import setup, find_packages
+
+cwd = os.path.abspath(os.path.dirname(__file__))
+
+def read(filename):
+    with codecs.open(os.path.join(cwd, filename), 'rb', 'utf-8') as h:
+        return h.read()
+
+metadata = read(os.path.join(cwd, 'pyley', '__init__.py'))
+
+def extract_metaitem(meta):
+    meta_match = re.search(r"""^__{meta}__\s+=\s+['\"]([^'\"]*)['\"]""".format(meta=meta),
+                           metadata, re.MULTILINE)
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError('Unable to find __{meta}__ string.'.format(meta=meta))
 
 setup(
-    name='pyley',
-    version='0.2.2',
-    author='Ziya SARIKAYA',
-    author_email='sarikayaziya@gmail.com',
-    packages=['pyley'],
-    url='https://github.com/ziyasal/pyley',
-    license='LICENSE',
-    description='Python client for an open-source graph database Cayley',
+    name='socket.io-emitter',
+    version=extract_metaitem('version'),
+    license=extract_metaitem('license'),
+    description=extract_metaitem('description'),
+    long_description=(read('README.rst')),
+    author=extract_metaitem('author'),
+    author_email=extract_metaitem('email'),
+    url=extract_metaitem('url'),
+    download_url=extract_metaitem('download_url'),
+    packages=find_packages(exclude=('tests')),
+    platforms=['Any'],
     install_requires=['requests'],
-    include_package_data=True
+    tests_require=['python-coveralls', 'coverage', 'nose'],
+    keywords='graph database, cayley, cayley python client, client',
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
